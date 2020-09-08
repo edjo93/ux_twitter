ec383de6ef9285d2287d09087ecc37a23a5166e3
edjo93 <eddas.carrasco@unitec.edu>
2020-09-08 14:43:06 -0600

    upload

--- src/App.js
+++ src/App.js
@@ -1,26 +1,105 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import'./custom.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+
+  render(){
+    
+    return (
+    
+
+      <div> 
+
+
+      
+
+
+        <nav class="navbar navbar-expand-md navbar-light bg-light fixed-top">
+          <a class="navbar-brand" href="#">Twitter</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+              </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+              <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search"/>
+              <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
+          </div>
+        </nav>
+
+
+
+
+        <main role="main" class="container">
+
+          <div class="starter-template">
+            <div class="row">
+              <div class="col-lg-4">
+                  <div class="row">
+                    <div class="col-lg-12">
+                      
+                      <div class="component text-center">
+                        <select class="form-control" id="slc-usuario">
+                          <option value="1">Lionel Messi</option>
+                          <option value="2">Iker Casillas</option>
+                        </select>
+                        <div id="div-usuarioactual">
+                            
+                        </div>
+                        
+                      </div>
+                    </div>
+                    <div class="col-lg-12">
+                      <div class="component text-left" >
+                        <h2>Trends</h2>
+                        <div id="div-trends">
+                          
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+              </div>
+              <div class="col-lg-8">
+                <div class="component  text-left">
+                  <textarea class="form-control" placeholder="What's happening" id="txt-texto"></textarea>
+                  <input type="text" class="form-control hashtags-input" placeholder="Hashtags" id="txt-hash"/>
+                  <button type="button" class="btn btn-primary" id="btn-nuevo">Tweet</button>
+                </div>
+
+                <div id="div-tweets">
+                  
+                  
+                  </div>
+              </div>
+
+              
+            </div>
+          </div>
+
+        </main>
+
+
+      
+      </div>
+
+    
+
+    );
+
+  }
+
+
+
+  
 }
 
 export default App;
